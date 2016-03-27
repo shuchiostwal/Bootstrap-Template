8ecdec07863680e9348ae0a4c13a3c16d6f85a29
shuchiostwal <shuchi.0911@gmail.com>
2016-03-27 22:28:58 +0530

    Bootstrap Template
    Template for a company designed via twitter bootstrap

---
+++ animate.js
@@ -0,0 +1,12 @@
+$ function(){
+
+$("glyphicon glyphicon-briefcase animated shake").click(function(){
+
+$("glyphicon glyphicon-briefcase animated shake").animate({left:'100px'});
+
+});
+
+
+});
+
+
